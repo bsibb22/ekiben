6a0f90942b9f6f9692aecf51e8501b77d76f198c
Brandon S <116681127+bsibb22@users.noreply.github.com>
2024-11-24 22:30:01 -0500

    Added code to generate .db file

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import pandas
-import sqlite3
+import sqlite3 as sql
 
+# create the database
+conn = sql.connect('train_db.db')
+
+# get the raw data
+train_data = pandas.read_csv('join20240426.csv')
+train_data.to_sql('Connections', conn, if_exists='replace', index=False)
+
+train_data = pandas.read_csv('station20240426.csv')
+train_data.to_sql('Stations', conn, if_exists='replace', index=False)
+
+train_data = pandas.read_csv('line20240426.csv')
+train_data.to_sql('Lines', conn, if_exists='replace', index=False)
+
+train_data = pandas.read_csv('company20240328.csv')
+train_data.to_sql('Companies', conn, if_exists='replace', index=False)
+
+cur = conn.cursor()
+conn.close()
